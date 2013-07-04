e084038b1a30fe6ce3d1c775c5ccb2a7e609972a
bin liu <liubin0329@gmail.com>
2013-07-04 11:15:10 +0900

    first exec

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 for-github-api-test
 ===================
+
+1. gem install octokit
+
+2. exec in shell:
+$ export GITHUB_LOGIN=liubin0329
+$ export GITHUB_TOKEN=18ed
+
+3. ./test.rb
--- test.rb
+++ test.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts 'ss'
+require 'octokit'
+
+login = ENV['GITHUB_LOGIN']
+token = ENV['GITHUB_TOKEN']
+client = Octokit::Client.new(:login => login, :oauth_token => token)
+
+# puts self info
+puts client.user login
