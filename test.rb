2a231ad53ef9f459e268521e9b239c84b3973ff5
bin liu <liubin0329@gmail.com>
2013-07-04 11:29:00 +0900

    add branch1

--- test.rb
+++ test.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
 require 'octokit'
+require 'pp'
 
 login = ENV['GITHUB_LOGIN']
 token = ENV['GITHUB_TOKEN']
 client = Octokit::Client.new(:login => login, :oauth_token => token)
 
 # puts self info
-puts client.user login
+me = client.user login
+#pp me
+
+branches = client.branches 'liubin0329/for-github-api-test'
+
+pp branches
